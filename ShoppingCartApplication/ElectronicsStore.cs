b8f56e5b409c11d66c08b68aec8ffcdfdb4dda11
saja123 <saja_fawaz@yahoo.com>
2024-07-15 15:54:24 +0300

    solve cart

---
+++ ShoppingCartApplication/ElectronicsStore.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApplication
+{
+    public class ElectronicsStore
+    {
+        public ProductGenerator productGenerator = new ProductGenerator();
+
+        public List<Product> AvailableProducts = new List<Product>();
+
+        public ElectronicsStore()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                AvailableProducts.Add(productGenerator.GenerateProduct());
+            }
+        }
+
+        public void DisplayProducts()
+        {
+            int i = 0;
+            foreach (var product in AvailableProducts)
+            {
+                i++;
+                Console.WriteLine($"{i}. {product.Name} - {product.Price:F2} - {product.Category}");
+            }
+        }
+
+        public Product SelectProduct(int index)
+        {
+            if (index < 0 || index >= AvailableProducts.Count)
+            {
+                return null;
+            }
+            return AvailableProducts[index];
+        }
+    }
+}
---
+++ ShoppingCartApplication/ProductGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApplication
+{
+    public class ProductGenerator
+    {
+        public static Random random = new Random();
+        public static string[] productNames = { "Apple", "T-Shirt", "Laptop", "Banana", "Jeans" };
+
+        public Product GenerateProduct()
+        {
+            string name = productNames[random.Next(productNames.Length)];
+            int price = (int)(random.Next(100, 10000) / 100);
+            ProductCategory category = (ProductCategory)random.Next(ProductCategory.GetValues(typeof(ProductCategory)).Length);
+
+            return new Product(name, price, category);
+        }
+    }
+}
